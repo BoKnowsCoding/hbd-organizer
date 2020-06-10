31ce366d829795111f838b85344ee072df0da063
Bo Cameron <bogarrettcameron@gmail.com>
2020-06-09 21:57:15 -0400

    Added the book-copier to the shell script

--- hbd-runner.sh
+++ hbd-runner.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# Replace the download locations with your own.
+# Replace these values as needed.
 
 HBD_DOWNLOAD_PATH="/mnt/storage/archive/humble"
+
+# books will be directly in this "new" folder, to be imported into calibre manually
+BOOK_DEPLOYMENT_PATH="/mnt/storage/media/books/new"
+
+# the comics directory will be under this one, along with manga, e.g. /books/comics and /books/manga
 COMIC_DEPLOYMENT_PATH="/mnt/storage/media/books"
+
+# all output of the python scripts is output to this log, and refreshed each time hbd-runner.sh runs
 LOG_PATH="/mnt/storage/.logs"
 COOKIE_FILE="./hbd-cookies.txt"
 
 # change to directory of this script
 cd "$(dirname "$0")"
 hbd download --cookie-file "$COOKIE_FILE" --library-path "$HBD_DOWNLOAD_PATH" --progress &> "$LOG_PATH/hbd.log"
-python3 ./comic-picker.py "$HBD_DOWNLOAD_PATH" "$COMIC_DEPLOYMENT_PATH" &> "$LOG_PATH/comic-picker.log"
+python3 ./book-copier.py "$HBD_DOWNLOAD_PATH" "$BOOK_DEPLOYMENT_PATH" &>> "$LOG_PATH/hbd.log"
+python3 ./comic-picker.py "$HBD_DOWNLOAD_PATH" "$COMIC_DEPLOYMENT_PATH" &>> "$LOG_PATH/hbd.log"
