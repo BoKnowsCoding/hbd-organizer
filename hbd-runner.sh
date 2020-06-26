eebc2cb95ae743b6a46930244d19e237af316796
Bo Cameron <bogarrettcameron@gmail.com>
2020-06-26 16:04:25 -0400

    Now checks if another instance is running

--- hbd-runner.sh
+++ hbd-runner.sh
@@ -19,7 +19,10 @@ COOKIE_FILE="./hbd-cookies.txt"
 
 # change to directory of this script
 cd "$(dirname "$0")"
-hbd download --cookie-file "$COOKIE_FILE" --library-path "$HBD_DOWNLOAD_PATH" --progress &> "$LOG_PATH/hbd.log"
-python3 ./book-copier.py "$HBD_DOWNLOAD_PATH" "$BOOK_DEPLOYMENT_PATH" &>> "$LOG_PATH/hbd.log"
-python3 ./comic-picker.py "$HBD_DOWNLOAD_PATH" "$COMIC_DEPLOYMENT_PATH" &>> "$LOG_PATH/hbd.log"
-python3 ./audio-extractor.py "$HBD_DOWNLOAD_PATH" "$MUSIC_DEPLOYMENT_PATH" "$AUDIOBOOK_DEPLOYMENT_PATH" &>> "$LOG_PATH/hbd.log"
+(
+flock -n 456 || exit 1
+    hbd download --cookie-file "$COOKIE_FILE" --library-path "$HBD_DOWNLOAD_PATH" --progress &> "$LOG_PATH/hbd.log"
+    python3 ./book-copier.py "$HBD_DOWNLOAD_PATH" "$BOOK_DEPLOYMENT_PATH" &>> "$LOG_PATH/hbd.log"
+    python3 ./comic-picker.py "$HBD_DOWNLOAD_PATH" "$COMIC_DEPLOYMENT_PATH" &>> "$LOG_PATH/hbd.log"
+    python3 ./audio-extractor.py "$HBD_DOWNLOAD_PATH" "$MUSIC_DEPLOYMENT_PATH" "$AUDIOBOOK_DEPLOYMENT_PATH" &>> "$LOG_PATH/hbd.log"
+) 456>/var/lock/hbd-runner-lock
